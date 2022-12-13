02e0c7c91a575d42ff0b8e8876d0cda6ab8238df
Matin <askarimatin79@gmail.com>
2022-12-13 22:19:07 +0330

    question number 1 solved

--- question-1/main.js
+++ question-1/main.js
@@ -1,1 +1,20 @@
-console.log(1);
+var list = [1, 2, [3, 4, [6, 7], 8], 5];
+
+var depth = 0;
+
+function arrayDepth(array) {
+  if (Array.isArray(array)) {
+    ++depth;
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      if (Array.isArray(element)) {
+        let arr = array.flat();
+        arrayDepth(arr);
+      }
+    }
+    return depth;
+  }
+  throw new Error("Input must be array");
+}
+
+console.log(arrayDepth(list));
